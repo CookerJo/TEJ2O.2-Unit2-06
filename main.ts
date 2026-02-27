54688ee3758fb149db6fb57665fa776e23f26e1c
Josiah-Rigg <josiah.rigg@stu.ocsb.ca>
2026-02-27 13:28:40 -0500

    Changed comment so when A is pressed turns on B turns off.

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Josiah
+ * Created on: Feb 2026
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+pins.digitalWritePin(DigitalPin.P16, 0) // this turns the pin off
+
+// turns light on
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// turns light off
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
